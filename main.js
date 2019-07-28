d755d1d99b23686bed4bb498495bf5931b64a368
Buse Yaren Tekin <tekinyrnn35@gmail.com>
2019-07-28 09:23:18 -0300

    Update main.js

--- main.js
+++ main.js
@@ -9,10 +9,6 @@ const posts =[
   }
 ]
 
-function getPosts() {
-  console.log(posts);
-}
-
 
 function createPost(post,callback,callback2,callback3) {
   setTimeout(
@@ -20,18 +16,26 @@ function createPost(post,callback,callback2,callback3) {
       posts.push(post)
       callback()
       callback2()
-      callback3(posts[2])
+      callback3(posts[2],
+      "Ben değişen bir başlığım",
+      "Ben değişen bir body mesajıyım")
     }, 5000 )
 }
 
 //3 tane callback olacak ismi update post get post yaptıktan sonra postlardan 1 tanesini update edecek
 
-function updatePostWithoutArgument() {
-  posts[0].title ="Başlık 1 Yenilendi"
+function getPosts() {
+  console.log(posts);
 }
 
-function updatePostWithArguments(post) {
-  post.title ="Başlık Yeni Hali"
+function updatePostWithArguments(post, message, bodyMessage) {
+  // post.title ="Başlık Yeni Hali"
+  post.title = message
+  post.body = bodyMessage
+}
+
+function updatePostWithoutArgument() {
+  posts[0].title ="Başlık 1 Yenilendi"
 }
 
 createPost(
@@ -43,3 +47,4 @@ createPost(
   updatePostWithoutArguments,
   updatePostWithArgument
 )
+
